d640d5228ae480799109120d90614d8320d6f015
aitch_nu <harshinlko@gmail.com>
2012-08-02 22:29:25 +0530

    no msg

--- Box2d world/src/com/box2d/main/GlobalSettings.java
+++ Box2d world/src/com/box2d/main/GlobalSettings.java
@@ -3,8 +3,8 @@ package com.box2d.main;
 
 public class GlobalSettings {
 	
-	public static final int SCREEN_WIDTH=1280;
-	public static final int SCREEN_HEIGHT=700;
+	public static final int SCREEN_WIDTH=320;
+	public static final int SCREEN_HEIGHT=240;
 	public static int GROUND_HEIGHT=129;
 	public static final float BOX_STEP=1/60f;
 	public static final int VELOCITY_ITERATIONS=8;
